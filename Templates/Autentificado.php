4feb054c519cfa590a65ee50754f788c68340df9
DragonKnight <castilloc185@gmail.com>
2011-10-18 13:06:33 -0430

    Modificados todos archivos implicados en el proceso de login, se agrego el archivo de cerrar sesión, pero aun no es funcional la utilidad (hay que analizar la estructura de los session_start); se modifico y definio el nuevo estilo para el escritorio y se aligero la cantidad de archivos al retirar menuautentif y escritoriosDevel

--- Templates/Autentificado.php
+++ Templates/Autentificado.php
@@ -96,39 +96,38 @@
 				<div id="Contenido">
 					<?php 
 						//incluyo el archivo de menu's (menu.php)
-						include ("escritorios.php");
 		
 						if($_SESSION['idCargo']==0) 
 						{							
-							escritorioAdministrador();
+							include ("escAdmin.php");
 						}
 						if($_SESSION['idCargo']==1) 
 						{							
-							escritorioSG();
+							
 						}
 						if($_SESSION['idCargo']==2) 
 						{							
-							escritorioCoordinador();
+							
 						}
 						if($_SESSION['idCargo']==3) 
-						{							
-							escritorioGacetas();
-						}
+						{
+													
+						}	
 						if($_SESSION['idCargo']==4) 
 						{							
-							escritorioAudiencias();
+							
 						}
 						if($_SESSION['idCargo']==5) 
 						{							
-							escritorioAsist();
+							
 						}
 						if($_SESSION['idCargo']==6) 
 						{							
-							escritorioSecr();
+							
 						}
 						if($_SESSION['idCargo']==7) 
 						{							
-							escritorioRecepcionista();
+
 						}
 					?>
 				</div>
@@ -151,8 +150,79 @@
 				</div>
 			</div>
 		</html>
-<?php
-		
+<?php		
 	}
-?>
-	
+	else 
+	{
+?>	
+		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+			<head>
+				<title>Sistema de Correspondencia -- SISCO -- </title>
+				<meta http-equiv="Content-Type" content="application/xhtml+xml;charset=utf-8" />
+				<meta http-equiv="Cache-Control" content="max-age=86400" />
+				<meta content='width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;' name='viewport' />
+				<link rel="stylesheet" type="text/css" href="../Static/Css/Principal.css" media="all" />
+				<link rel="stylesheet" type="text/css" href="../Libs/ddlevelsfiles/ddlevelsmenu-base.css" />
+				<link rel="stylesheet" type="text/css" href="../Libs/ddlevelsfiles/ddlevelsmenu-topbar.css" />
+		
+				<script type="text/javascript" src="../Libs/ddlevelsfiles/ddlevelsmenu.js">
+					/***********************************************
+					* All Levels Navigational Menu- (c) Dynamic Drive DHTML code library (http://www.dynamicdrive.com)
+					* This notice MUST stay intact for legal use
+					* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+					***********************************************/
+				</script>		
+				<script type="text/javascript" src="../Static/Js/funciones.js"></script> <! -- Funciones js del sistema -- >
+			</head>
+			<div id="Contenedor">
+				<div id="Cabecera">
+					<div id="columna1">
+						<img src="../Static/Images/escudo.gif" alt="EscudoMerida" >
+					</div>
+					<div id="columna2">
+						<div id="TituloCabecera">
+							Gobernación del Estado Mérida  <br />
+							Secretaria General de Gobierno
+						</div>
+					</div>
+					<div id="columna3">
+						<img src="../Static/Images/bandera.gif" alt="BanderaMerida" >
+					</div>
+				</div>
+				<div id="Menu">
+					<?php 
+						//incluyo el archivo de menu's (menu.php)
+						include ("./menu.php");	
+						menuNoAutent();
+					?>
+				</div>
+				<div id="Contenido">
+					<div id="errorLogin">
+						<p>
+							Error, no ha iniciado Sesión, o su Sesión en el sistema de Correspondencia Sisco a caducado;
+						</p>
+					</div>
+				</div>
+				<div id="Powered">
+					<hr>
+					<div id="Pcolumna1">
+						Este sitio emplea:
+					</div>
+					<div id="Pcolumna2">
+						<img src="../Static/Images/powered-by-debian-gnu-linux.png" alt="Debian" >
+					</div>
+					<div id="Pcolumna3">
+						<img src="../Static/Images/powered-by-apache2.gif" alt="Apache" >
+					</div>
+				</div>
+				<div id="Pie">
+					Secretaria General de Gobierno <br />
+					Entre Av. 3 y 4, Edificio del Palacio de Gobierno, Piso 1 <br />
+					Teléfonos:			
+				</div>
+			</div>
+		</html>
+<?php		
+	}
+?>	
--- Templates/escAdmin.php
+++ Templates/escAdmin.php
@@ -1,23 +1,17 @@
 <?php 
-	//session_start();
 	//verificamos que el usuario que llama tenga los privilegios necesarios:
 ?>
-	<div id="resumen">
-		<div id="CasillaId">
-			<p>
-				Usuario:<?php echo " ".$_SESSION['usuario'] ?> <br />
-				Nivel:<?php echo " ".$_SESSION['idCargo'] ?> 
-			</p>
-		</div>
-		<div id="resumenTit">
-			<h2>Resumen de Operaciones:</h2>
-		</div>
-		<div id="resumenFecha">
-			Hoy es:
-		</div>
-		<script type="text/javascript">
-			consultaBit('*','','resumenCont');
-		</script>
-		<div id="resumenCont">
-		</div>
+
+	<div id="CasillaId">
+		<p>
+			Usuario:<?php echo " ".$_SESSION['usuario'] ?> <br />
+			Nivel:<?php echo " ".$_SESSION['idCargo'] ?> 
+		</p>
 	</div>
+	<div id="resumenTit">
+		<h2>Resumen de Operaciones:</h2>
+	</div>
+	<div id="resumenCont">
+		<p id="subTitulo">Log del sistema:</p> 
+	</div>
+
