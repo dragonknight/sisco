5387a34c555c86e3e0c8d77368c43467ff5e7d77
dragonknight <castilloc185@gmail.com>
2011-10-22 00:58:55 -0430

    Iniciado el desarrollo de templates que verifiquen el nivel de acceso para mostarse, de esta manera se aumenta la seguridad del sistema ya que solo un usuario con login valido y nivel adecuado puede acceder al contenido. De esta funcionalidad solo falta la vinculación con la bitacora del sistema

--- Templates/escAdmin.php
+++ Templates/escAdmin.php
@@ -1,17 +1,35 @@
 <?php 
 	//verificamos que el usuario que llama tenga los privilegios necesarios:
+	if($_SESSION['idCargo']=='0')
+	{
 ?>
 
-	<div id="CasillaId">
+		<div id="CasillaId">
+			<p>
+				Usuario:<?php echo " ".$_SESSION['usuario'] ?> <br />
+				Nivel:<?php echo " ".$_SESSION['idCargo'] ?> 
+			</p>
+		</div>
+		<div id="resumenTit">
+			<h2>Resumen de Operaciones:</h2>
+		</div>
+		<div id="resumenCont">
+			<p id="subTitulo">Log del sistema:</p> 
+		</div>
+<?php
+	} 
+	else 
+	{
+		// si no posee los privilegios necesarios, alertamos un intento de escalada de privilegios de un usuario debidamente logeado:
+?>
+		<script type="text/javascript" src="../Static/Js/funciones.js"> 
+			bitacora('Intento de escalar privilegios','7');
+		</script> 
+		
 		<p>
-			Usuario:<?php echo " ".$_SESSION['usuario'] ?> <br />
-			Nivel:<?php echo " ".$_SESSION['idCargo'] ?> 
+			Error, la pagina que ha solicitado no esta disponible para su nivel de acceso, <br />
+			Este incidente ha sido reportado al administrador del sistema...
 		</p>
-	</div>
-	<div id="resumenTit">
-		<h2>Resumen de Operaciones:</h2>
-	</div>
-	<div id="resumenCont">
-		<p id="subTitulo">Log del sistema:</p> 
-	</div>
-
+<?php
+	}
+?>
