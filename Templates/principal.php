e2aa81c637977020be5fd015d103983d75ff604d
DragonKnight <castilloc185@gmail.com>
2011-10-27 14:35:35 -0430

    modificada la página de Desarrolladores, se a adaptado reflejar correctamente la adopción de la Licencia Pública General (GNU/GPL)

--- Templates/principal.php
+++ Templates/principal.php
@@ -1,5 +1,7 @@
 <!-- “This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.“. -->
 
-Bienvenido al Sistema de Correspondencia de la Secretaria General de Gobierno Sisco... <br />
-Para hacer uso de esta Aplicación por favor seleccione la opción Ingresar del menú ubicado en la parte superior<br /><br />
-Si pertenece a la Secretaria, pero no le han asignado un usuario, por favor comuníquese con el administrador del sistema
+<p>
+	Bienvenido al Sistema de Correspondencia de la Secretaria General de Gobierno Sisco... <br />
+	Para hacer uso de esta Aplicación por favor seleccione la opción Ingresar del menú ubicado en la parte superior<br /><br />
+	Si pertenece a la Secretaria, pero no le han asignado un usuario, por favor comuníquese con el administrador del sistema
+</p>
