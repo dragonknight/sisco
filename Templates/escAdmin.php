9ff98e1530a64990f74aee7d6be6e7ef9c32d569
DragonKnight <root@Ferb.(none)>
2011-12-12 17:19:55 -0430

    *) Eliminado el archivo de logBitacora.php, ya que no era necesario, su contenido se paso directamente al escritorio del administrador y funciona correctamente asi y presenta mejor estructura al proyecto

--- Templates/escAdmin.php
+++ Templates/escAdmin.php
@@ -15,10 +15,35 @@
 			<h2>Resumen de Operaciones:</h2>
 		</div>
 		<div id="resumenCont">
-			<?php
-				include ("./logBitacora.php");
-				bitTacoraCompleta();
-			?>
+			<h4>Log General del sistema:</h4>
+<?php
+				include ("./funciones.php");
+				$resumen = consulta('*','bitacora','','idTransaccion');
+				$i = $resumen[1]-1;
+?>
+			<div id="izq">
+				<?php echo 'Número de entradas en el log: '. $resumen[1] . '<br /><br />'; ?>
+			</div>
+			<div id="bitCol1">
+				<strong> Nº: </strong><br />
+				<?php echo $resumen[0][$i][0]; ?> <br />
+			</div>
+			<div id="bitCol2">
+				<strong> Tipo: </strong><br />
+				<?php echo $resumen[0][$i][1]; ?> <br />
+			</div>
+			<div id="bitCol3">
+				<strong> Detalle: </strong><br />
+				<?php echo $resumen[0][$i][2]; ?> <br />
+			</div>
+			<div id="bitCol4">
+				<strong> IP: </strong><br />
+				<?php echo $resumen[0][$i][3]; ?> <br />
+			</div>
+			<div id="bitCol5">
+				<strong> Fecha: </strong><br />
+				<?php echo $resumen[0][$i][4]; ?> <br />
+			</div>		
 		</div>
 <?php
 	}
