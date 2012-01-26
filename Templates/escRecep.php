9887f3702d6229dc61bfaf38c27aaf3a249aac89
DK <castilloc185@gmail.com>
2012-01-26 13:00:19 -0430

    *) corregido el cuarto error de la revisión del 2301 referente a los indices de comunicaciones en el escritorio del recepcionista

--- Templates/escRecep.php
+++ Templates/escRecep.php
@@ -11,25 +11,31 @@
 			<h4>Comunicaciones Recibidas:</h4><br />
 <?php
 				include ("./funciones.php");
-				$hoy = cuenta_reg('comunicaciones','*','TDirec = \'E\''); //La condicion es que Tdirec = E && fecha = hoy
-				$semana = cuenta_reg('comunicaciones','*','TDirec = \'E\''); //La condicion es que Tdirec = E && fecha = hoy
-				$mes = cuenta_reg('comunicaciones','*','TDirec = \'E\''); //La condicion es que Tdirec = E && fecha = hoy
-				$total = cuenta_reg('comunicaciones','*','TDirec = \'E\''); //La condicion es que Tdirec = E
+				$estandar = cuenta_reg('comunicaciones','*','tDirec = \'Entrante\' && tCom = \'Estandar\' ');
+				$gacetas = cuenta_reg('comunicaciones','*','tDirec = \'Entrante\' && tCom = \'Gaceta\' ');
+				$audiencias = cuenta_reg('comunicaciones','*','tDirec = \'Entrante\' && tCom = \'Audiencia\' ');
+				$invitaciones = cuenta_reg('comunicaciones','*','tDirec = \'Entrante\' && tCom = \'Invitación\' ');
+				$denuncias = cuenta_reg('comunicaciones','*','tDirec = \'Entrante\' && tCom = \'Denuncia\' ');
+				$total = cuenta_reg('comunicaciones','*','tDirec = \'Entrante\'');
 				
 ?>
 			<div id="rCol1">
-				<strong> Hoy: </strong><br />
-				<?php echo $hoy[0]; ?>
+				<strong> Estandar: </strong><br />
+				<?php echo $estandar[0]; ?><br /><br /> 
+				<strong> Denuncia: </strong><br />
+				<?php echo $denuncias[0]; ?><br /><br />
 			</div>
 			<div id="rCol2">
-				<strong> Semana: </strong><br />
-				<?php echo $semana[0]; ?>
+				<strong> Gaceta: </strong><br />
+				<?php echo $gacetas[0]; ?><br /><br /><br /><br /><br />
 			</div>
 			<div id="rCol3">
-				<strong> Mes: </strong><br />
-				<?php echo $mes[0]; ?>
+				<strong> Audiencia: </strong><br />
+				<?php echo $audiencias[0]; ?><br /><br /><br /><br /><br />
 			</div>
 			<div id="rCol4">
+				<strong> Invitación: </strong><br />
+				<?php echo $invitaciones[0]; ?><br /><br /><br /><br /><br />
 				<strong> Total: </strong><br />
 				<?php echo $total[0]; ?>
 			</div>
